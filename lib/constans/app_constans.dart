c322c9c0dddbc307f8526264b2ab786be0836071
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-10 22:04:08 +0300

    App constans

---
+++ lib/constans/app_constans.dart
@@ -0,0 +1,15 @@
+
+import 'package:ecommerce_flutter/services/assets_manages.dart';
+
+class AppConstans {
+
+  static List<String> bannerImages=[
+    AssetsManager.slide1,
+    AssetsManager.slide2,
+    AssetsManager.slide3,
+    AssetsManager.slide4,
+    AssetsManager.slide5,
+    AssetsManager.slide6,
+    AssetsManager.slide7,
+  ];
+}
