8df1b0c293d295800cfd7f80309e57652e4a1ca3
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-11 15:50:21 +0300

    Product Detail 2

--- lib/widgets/products/product_details.dart
+++ lib/widgets/products/product_details.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce_flutter/constans/app_constans.dart';
+import 'package:ecommerce_flutter/widgets/app_name_text.dart';
+import 'package:ecommerce_flutter/widgets/subtitle_text.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatefulWidget {
@@ -11,6 +15,92 @@ class ProductDetailScreen extends StatefulWidget {
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size  = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: (){
+            if(Navigator.canPop(context)){
+              Navigator.pop(context);
+            }
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+          ),
+        ),
+        title: const AppNameTextWidget(fontSize: 20,),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FancyShimmerImage(
+                imageUrl: AppConstans.imageUrl,
+                height: size.height * 0.35,
+                width: double.infinity,
+
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: Text(
+                            "Title"*20,
+                          softWrap: true,
+                          style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500)
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                      const SubTitleTextWidget(
+                          label: "\$1000.00",
+                        fontSize: 20,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.red,
+                      )
+
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            "kalp"
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Expanded(child: SizedBox(
+                          height: kBottomNavigationBarHeight -10,
+
+                        ))
+                      ],
+
+                    ),
+
+                  )
+                ],
+              )
+            ),
+          ],
+        ),
+
+      ),
+    );
   }
 }
