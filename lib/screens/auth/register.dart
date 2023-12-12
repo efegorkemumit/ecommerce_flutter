3e30e216b31413541d9525e55939ea34d596dd1a
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-12 16:40:31 +0300

    register 2

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -13,6 +13,59 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  bool obscureText =true;
+  late final TextEditingController _nameController,
+      _emailController,
+      _passwordController,
+      _repeatPasswordController;
+
+  late final FocusNode _nameFocusNode, _emailFocusNode, _passwordFocusNode,
+    _repeatPasswordFocuNode;
+
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  void initState(){
+    _nameController = TextEditingController();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _repeatPasswordController = TextEditingController();
+
+    _nameFocusNode = FocusNode();
+    _emailFocusNode = FocusNode();
+    _passwordFocusNode = FocusNode();
+    _repeatPasswordFocuNode = FocusNode();
+
+    super.initState();
+
+  }
+
+  @override
+  void dispose(){
+    if(mounted){
+
+      _nameController.dispose();
+      _emailController.dispose();
+      _passwordController.dispose();
+      _repeatPasswordController.dispose();
+
+      _nameFocusNode.dispose();
+      _emailFocusNode.dispose();
+      _passwordFocusNode.dispose();
+      _repeatPasswordFocuNode.dispose();
+
+
+    }
+    super.dispose();
+
+  }
+
+  Future<void> registerFCT() async{
+    final isValid = _formkey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
