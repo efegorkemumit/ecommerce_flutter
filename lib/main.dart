d941bdf2457217fc8cb9fbaec32f05935322715e
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-13 11:59:39 +0300

    Uuid

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,8 @@ class MyApp extends StatelessWidget {
           title: 'Eccomerce Ap ',
           theme: Styles.themeData(isDarkTheme: themeProvider.getIsDarkTheme, context: context),
 
-         home:const LoginScreen(),
-          // home:const RootScreen(),
+     //    home:const LoginScreen(),
+          home:const RootScreen(),
           routes: {
             ProductDetailScreen.routName : (context)=> const ProductDetailScreen(),
             WishlistScreen.routName : (context)=> const WishlistScreen(),
