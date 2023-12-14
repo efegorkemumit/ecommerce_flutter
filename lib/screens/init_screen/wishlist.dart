3c16900cfbbf3b5943da70cbabbca117cf9b3ccc
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-14 22:40:29 +0300

    Wishlist 2

--- lib/screens/init_screen/wishlist.dart
+++ lib/screens/init_screen/wishlist.dart
@@ -1,10 +1,14 @@
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
+import 'package:ecommerce_flutter/providers/wishlist_provider.dart';
 import 'package:ecommerce_flutter/screens/cart/bottom_checkout.dart';
 import 'package:ecommerce_flutter/services/assets_manages.dart';
 import 'package:ecommerce_flutter/widgets/app_name_text.dart';
 import 'package:ecommerce_flutter/screens/cart/cart_widget.dart';
 import 'package:ecommerce_flutter/screens/cart/empty_bag.dart';
+import 'package:ecommerce_flutter/widgets/products/product_widget.dart';
 import 'package:ecommerce_flutter/widgets/title_text.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WishlistScreen extends StatelessWidget {
   static const routName = "/WishlistScreen";
@@ -15,7 +19,9 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return isEmpty ?
+    final wishListProvider = Provider.of<WishlistProvider>(context);
+
+    return wishListProvider.getWishLists.isEmpty ?
     Scaffold(
         appBar: AppBar(
             centerTitle: true,
@@ -52,11 +58,24 @@ class WishlistScreen extends StatelessWidget {
             size: 20,
           ),
         ),
-        title: const TitleTextWidget(label: "Wishlist")
+        title:  TitleTextWidget(label: "Wishlist ${wishListProvider.getWishLists.length}")
       ),
-      body: ListView.builder(itemBuilder: (context, index){
-        return const CardWidget();
-      }),
+      body: DynamicHeightGridView(
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        builder: (context,index){
+          return Padding(
+          padding: const EdgeInsets.all(8.0),
+            child: ProductWidget(
+              productId: wishListProvider.getWishLists.values.toList()[index].productId,
+
+            ),
+          );
+
+        },
+      itemCount: wishListProvider.getWishLists.length,
+      crossAxisCount: 2,
+      )
 
     );
   }
