e5a4b40ad3cf358a5f328e53f0119cca1384b8e9
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-18 16:06:37 +0300

    google_btn

--- lib/main.dart
+++ lib/main.dart
@@ -86,8 +86,8 @@ class MyApp extends StatelessWidget {
           title: 'Eccomerce Ap ',
           theme: Styles.themeData(isDarkTheme: themeProvider.getIsDarkTheme, context: context),
 
-      //  home:const LoginScreen(),
-          home:const RootScreen(),
+        home:const LoginScreen(),
+      //    home:const RootScreen(),
           routes: {
             ProductDetailScreen.routName : (context)=> const ProductDetailScreen(),
             RootScreen.routName : (context)=> const RootScreen(),
